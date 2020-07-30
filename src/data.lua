e2129a29337aa5ac1d5881b661be33088725f62a
Dmitry R <dimanius851@gmail.com>
2020-07-31 00:00:16 +0300

    move to 0.18.x

--- src/data.lua
+++ src/data.lua
@@ -1,37 +1,25 @@
 -- override shoot sound for laser turrets
-if not settings.startup["pew-argh-laser-turret-vanilla"].value then
-  data.raw["electric-turret"]["laser-turret"].attack_parameters.sound = {}
-end
 if settings.startup["pew-argh-laser-turret-pew"].value then
-  sound = data.raw["electric-turret"]["laser-turret"].attack_parameters.sound
-  sound[#sound+1] = { filename = "__PEW-ARGH__/sounds/pew.ogg", volume = 1 }
-end
-if settings.startup["pew-argh-laser-turret-pew2"].value then
-  sound = data.raw["electric-turret"]["laser-turret"].attack_parameters.sound
-  sound[#sound+1] = { filename = "__PEW-ARGH__/sounds/pew2.ogg", volume = 1 }
+  data.raw.beam["laser-beam"].working_sound =
+      {sound = {filename = "__PEW-ARGH__/sounds/pew.ogg", volume = 1}}
 end
 
 -- override shoot sound for gun turrets
-if settings.startup["pew-argh-gun-turret-bam"].value then
-  data.raw["ammo-turret"]["gun-turret"].attack_parameters.sound = {
-  	{
-    	filename = "__PEW-ARGH__/sounds/bambambam.ogg",
-  		volume = 1
-  	}
-  }
-end
-
+-- if settings.startup["pew-argh-gun-turret-bam"].value then
+--     data.raw["ammo-turret"]["gun-turret"].attack_parameters.sound =
+--         {{filename = "__PEW-ARGH__/sounds/bambambam.ogg", volume = 1}}
+-- end
 
 -- add wilhelm scream to existing list of dying sounds for biters
-if settings.startup["pew-argh-wilhelm"].value then
-  units = {"small-biter", "medium-biter", "big-biter", "behemoth-biter"}
-  wilhelm_scream = {
-    	filename = "__PEW-ARGH__/sounds/wilhelm.ogg",
-      -- wilhelm scream is never subtle
-      volume = 1
-  }
-  for key,value in pairs(units) do
-    sound = data.raw["unit"][value].dying_sound
-    sound[#sound+1] = wilhelm_scream
-  end
-end
+-- if settings.startup["pew-argh-wilhelm"].value then
+--     units = {"small-biter", "medium-biter", "big-biter", "behemoth-biter"}
+--     wilhelm_scream = {
+--         filename = "__PEW-ARGH__/sounds/wilhelm.ogg",
+--         -- wilhelm scream is never subtle
+--         volume = 1
+--     }
+--     for key, value in pairs(units) do
+--         sound = data.raw["unit"][value].dying_sound
+--         sound[#sound + 1] = wilhelm_scream
+--     end
+-- end
--- src/settings.lua
+++ src/settings.lua
@@ -1,32 +1,20 @@
 data:extend({
   {
     type = "bool-setting",
-    name = "pew-argh-laser-turret-vanilla",
-    setting_type = "startup",
-    default_value = false
-  },
-  {
-    type = "bool-setting",
     name = "pew-argh-laser-turret-pew",
     setting_type = "startup",
     default_value = true
   },
-  {
-    type = "bool-setting",
-    name = "pew-argh-laser-turret-pew2",
-    setting_type = "startup",
-    default_value = true
-  },
-  {
-    type = "bool-setting",
-    name = "pew-argh-gun-turret-bam",
-    setting_type = "startup",
-    default_value = true
-  },
-  {
-    type = "bool-setting",
-    name = "pew-argh-wilhelm",
-    setting_type = "startup",
-    default_value = true
-  }
+  -- {
+  --   type = "bool-setting",
+  --   name = "pew-argh-gun-turret-bam",
+  --   setting_type = "startup",
+  --   default_value = true
+  -- },
+  -- {
+  --   type = "bool-setting",
+  --   name = "pew-argh-wilhelm",
+  --   setting_type = "startup",
+  --   default_value = true
+  -- }
 })
